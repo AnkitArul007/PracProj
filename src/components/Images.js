9d41e4a79835c371f0158224293377999d3b00ef
AnkitArul007 <nktarul@gmail.com>
2023-04-07 20:19:30 +0530

    pagination effect

--- src/components/Images.js
+++ src/components/Images.js
@@ -3,24 +3,39 @@ import "./images.css";
 import Button from "./Button";
 import { imageData as Image } from "../utils/imageLink";
 
-const pageCount = Image.length / 10;
-
 const Images = () => {
+  const [page, setPage] = useState(1);
+
+  const selectPageHandler = (selectedPage) => {
+    setPage(selectedPage);
+  };
   return (
     <React.Fragment>
       <div className="container image-section-wrapper mt-5">
-        {Image.slice(10, 19).map((ele, index) => (
+        {Image.slice(page * 9 - 9, page * 9).map((ele, index) => (
           <div className="image-cont" key={index}>
             <img src={ele} alt="testingImages" />
           </div>
         ))}
       </div>
-      <div className="pagination-cont d-flex justify-content-center align-items-center mt-3">
-        <Button title={1} />
-        <Button title={2} />
-        <Button title={3} />
-        <Button title={4} />
-      </div>
+
+      {Image.length > 0 && (
+        <div className="pagination-cont w-100 border d-flex justify-content-center align-items-center gap-3 mt-3">
+          {[...Array(Image.length / 10)].map((_, i) => {
+            return (
+              <div
+                className={`pagination ${
+                  page === i + 1 ? "pagination__selected" : ""
+                }`}
+                onClick={() => selectPageHandler(i + 1)}
+                key={i}
+              >
+                {i + 1}
+              </div>
+            );
+          })}
+        </div>
+      )}
     </React.Fragment>
   );
 };
